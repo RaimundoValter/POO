78b0a39b757c3854c5ce124c7b5d8ded09dee821
zeguii <guilherme.lima85@outlook.com>
2026-05-28 23:22:16 -0300

    28/05

--- Guia3/src/pergunta.py
+++ Guia3/src/pergunta.py
@@ -1,4 +1,19 @@
 from typing import List, Tuple, Dict
+from abc import ABC, abstractmethod
 
-class Pergunta:
-    pass
+class Pergunta(ABC):
+    def __init__(self, texto: str, explicacao_geral: str):
+        self.texto = texto
+        self.explicacao_geral = explicacao_geral
+
+    @abstractmethod
+    def validarr_resposta(self, resposta: str) -> bool:
+        pass
+
+    @abstractmethod
+    def get_explicacao(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_tipo(self) -> str:
+        pass
--- Guia3/src/perguntamultiplaescolha.py
+++ Guia3/src/perguntamultiplaescolha.py
@@ -1,4 +1,20 @@
 from typing import List, Tuple, Dict
 
-class PerguntaMultiplaEscolha:
-    pass
+from Guia3.src.pergunta import Pergunta
+
+class PerguntaMultiplaEscolha(Pergunta):
+    def __init__(self, texto: str, explicacao_geral: str, alternativas: List[Dict[str, str]]):
+        super().__init__(texto, explicacao_geral)
+        self.alternativas = alternativas
+
+    def validar_resposta(self, resposta: str) -> bool:
+        for alternativa in self.alternativas:
+            if alternativa['texto'] == resposta:
+                return alternativa['correta']
+        return False
+
+    def get_explicacao(self) -> str:
+        return self.explicacao_geral
+
+    def get_tipo(self) -> str:
+        return "Multipla Escolha"
