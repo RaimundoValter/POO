86c8f62d985a389232c9506a0a000ff020eb2d31
zeguii <guilherme.lima85@outlook.com>
2026-05-28 23:22:07 -0300

    28/05

--- Guia3/src/perguntadiscursiva.py
+++ Guia3/src/perguntadiscursiva.py
@@ -1,4 +1,19 @@
 from typing import List, Tuple, Dict
 
-class PerguntaDiscursiva:
-    pass
+from Guia3.src.pergunta import Pergunta
+
+class PerguntaDiscursiva(Pergunta):
+    def __init__(self, texto: str, explicacao_geral: str, resposta_certa: str):
+        super().__init__(texto, explicacao_geral)
+        self.resposta_certa = resposta_certa
+
+    def validar_resposta(self, resposta: str) -> bool:
+        return resposta == self.resposta_certa
+
+    def get_explicacao(self) -> str:
+        return self.explicacao_geral
+
+    def get_tipo(self) -> str:
+        return "Discursiva"
+    
+    
